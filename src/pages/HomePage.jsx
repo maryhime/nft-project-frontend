9d9232d0d1b5424b264dc3721b188ec855386323
Mary Caseria <mtmc@newmediastaff.com>
2023-09-08 11:56:54 +0800

    refs #3 cleaned old files and set up config and images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,70 +1,22 @@
 import React from 'react'
 import NavbarComponent from '../Components/NavbarComponent';
 import HeroComponent from '../Components/HeroComponent';
-import PartnersComponent from '../Components/PartnersComponent';
-import ServicesComponent from '../Components/ServicesComponent';
-import CharactersComponent from '../Components/CharactersComponent';
+
 
 const HomePage = () => {
   return (
     <>
-      <div className="bg-[#171217] h-fit" >
+      <div className="bg-[url('/images/background.png')]" >
         <div className="relative z-50">
           <NavbarComponent />
         </div>
 
         <HeroComponent />
-        <PartnersComponent />
+        {/* <PartnersComponent />
         <ServicesComponent />
-        <CharactersComponent />
+        <CharactersComponent /> */}
 
-        <div className="absolute top-[-185px] left-[500px]">
-          <svg width="958" height="961" viewBox="0 0 958 961" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g opacity="0.5" filter="url(#filter0_f_125_4661)">
-              <circle cx="791.5" cy="169.5" r="215.5" stroke="#00B6FF" stroke-width="278" />
-            </g>
-            <defs>
-              <filter id="filter0_f_125_4661" x="0" y="-622" width="1583" height="1583" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-                <feGaussianBlur stdDeviation="218.5" result="effect1_foregroundBlur_125_4661" />
-              </filter>
-            </defs>
-          </svg>
-        </div>
-        <div className="absolute top-[400px] left-[-200px]">
-          <svg width="793" height="1583" viewBox="0 0 793 1583" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g opacity="0.5" filter="url(#filter0_f_125_4690)">
-              <circle cx="1.5" cy="791.5" r="215.5" stroke="#FF00E8" stroke-width="278" />
-            </g>
-            <defs>
-              <filter id="filter0_f_125_4690" x="-790" y="0" width="1583" height="1583" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-                <feGaussianBlur stdDeviation="218.5" result="effect1_foregroundBlur_125_4690" />
-              </filter>
-            </defs>
-          </svg>
-
-        </div>
-        <div className="absolute top-[1800px] left-[700px]">
-          <svg width="1112" height="1583" viewBox="0 0 1112 1583" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g opacity="0.5" filter="url(#filter0_f_125_4689)">
-              <circle cx="791.5" cy="791.5" r="215.5" stroke="#00B6FF" stroke-width="278" />
-            </g>
-            <defs>
-              <filter id="filter0_f_125_4689" x="0" y="0" width="1583" height="1583" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-                <feGaussianBlur stdDeviation="218.5" result="effect1_foregroundBlur_125_4689" />
-              </filter>
-            </defs>
-          </svg>
-
-        </div>
-
-
-
+ 
         {/* <div className="absolute -z-[100] mt-[-180px] h-[100vh] w-full">
         <img className=' w-[100%]' src={backgroundImage} alt="background image" />
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,81 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'sans': 'Montserrat, sans-serif',
+        'body': 'Montserrat, sans-serif',
         'heading': 'Bebas Neue, sans-serif',
       },
       colors: {
         'primary': '#FF00E8',
         'secondary': '#00B6FF',
-      }
+        'white': '#FDFBFA',
+      },
+      fontSize: {
+        'main-heading': ['96px', {
+          lineHeight: '116.7%',
+          letterSpacing: 'normal',
+          fontWeight: '400',
+          fontFamily: 'heading'
+        }],
+        'heading': ['48px', {
+          lineHeight: '56.016px',
+          letterSpacing: 'normal',
+          fontWeight: '400',
+        }],
+        'body-text': ['16px', {
+          lineHeight: 'normal',
+          letterSpacing: 'normal',
+          fontWeight: '300',
+        }],
+        'accordion-heading': ['18px', {
+          lineHeight: '27px',
+          letterSpacing: 'normal',
+          fontWeight: '600',
+        }],
+        'headings': ['', {
+          lineHeight: '',
+          letterSpacing: '',
+          fontWeight: '',
+        }],
+        'button': ['18', {
+          lineHeight: '26px',
+          letterSpacing: 'normal',
+          fontWeight: '700',
+        }],
+        'sub-heading': ['20px', {
+          lineHeight: 'normal',
+          letterSpacing: 'normal',
+          fontWeight: '300',
+        }],
+        'nav-item': ['16px', {
+          lineHeight: '24px',
+          letterSpacing: 'normal',
+          fontWeight: '300',
+        }],
+      },
+      gap: {
+        '56': '56px',
+        '48': '48px',
+        '24': '24px',
+      },
+      padding: {
+        'main-sides-desktop': '16.5%',
+        'main-sides-tablet': '10%',
+        'main-sides-phone': '4%',
+        '48': '48px',
+        '24': '24px',
+      },
+      boxShadow: {
+        'pink': '3px 16px 28px 0px rgba(255, 0, 232, 0.15), 7px -8px 40px 0px rgba(255, 0, 232, 0.15), 9px 1px 18px 0px rgba(255, 0, 232, 0.25), -17px 1px 45px 0px rgba(255, 0, 232, 0.15)',
+        'blue': '3px 16px 28px 0px rgba(0, 182, 255, 0.15), 7px -8px 40px 0px rgba(0, 182, 255, 0.15), 9px 1px 18px 0px rgba(0, 182, 255, 0.25), -17px 1px 45px 0px rgba(0, 182, 255, 0.15)',
+      },
+      borderRadius : {
+        'accordion' : '50px 50px 50px 0px',
+        'button' : '5px',
+        'card' : '50px',
+      },
+      height: {
+        'button': '56px',
+      },
     },
   },
   plugins: [],
