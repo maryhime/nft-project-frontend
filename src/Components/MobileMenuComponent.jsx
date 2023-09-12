606b5838277f1d35c7b69ed8264aaaeaab395fe2
Mary Caseria <mtmc@newmediastaff.com>
2023-09-12 16:20:36 +0800

    refs #4 added mobile menu

---
+++ src/Components/MobileMenuComponent.jsx
@@ -0,0 +1,28 @@
+import { XMarkIcon } from '@heroicons/react/24/solid'
+import React from 'react'
+import { NAVIGATION } from '../data'
+import { ButtonComponent } from './GenericComponent.jsx/ButtonComponent'
+
+export const MobileMenuComponent = ({ close }) => {
+  return (
+    <div className="absolute z-50 top-0 w-screen bg-[#171217] h-screen px-main-sides-phone py-main-sides-phone flex flex-col gap-64">
+      <div className="flex justify-end">
+        <XMarkIcon onClick={close} className='h-[32px] w-[32px] text-white' />
+      </div>
+      <div className="flex flex-col gap-48">
+          <ul className='text-[20px] text-white font-body  uppercase flex flex-col justify-center gap-24 '>
+        {NAVIGATION.map((value, index) =>
+          <li key={index} className='hover:text-primary hover:cursor-pointer text-center'>
+            <a href=""></a>  {value}
+          </li>
+        )}
+      </ul>
+      <div className="flex justify-center">
+        <ButtonComponent title={'Buy Now'} variant={'filled'} style={'w-[180px]'} />
+      </div>
+      </div>
+    
+
+    </div>
+  )
+}
--- src/Components/NavbarComponent.jsx
+++ src/Components/NavbarComponent.jsx
@@ -1,29 +1,35 @@
-import React, { useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import { Bars3Icon } from '@heroicons/react/24/solid'
 import { ButtonComponent } from './GenericComponent.jsx/ButtonComponent';
 import { NAVIGATION } from '../data';
+import { MobileMenuComponent } from './MobileMenuComponent';
 
 export const NavbarComponent = () => {
+  const [openNav, setOpenNav] = useState(false);
 
   return (
-    <div className="flex flex-row gap-48 px-main-sides-phone md:px-main-sides-tablet 2xl:px-main-sides-desktop py-16 w-full backdrop-blur-md sticky top-0 z-50">
-      <div className="flex items-center">
-        <img src="images/logo.svg" className='w-[200px]' alt="logo" />
-      </div>
-      <div className=" text-white w-full flex items-center justify-end gap-48">
-        <ul className='hidden text-b font-body md:flex flex-row gap-24 text-nav-item '>
-          {NAVIGATION.map((value, index) =>
-            <li key={index} className='hover:text-primary hover:cursor-pointer'>
-            <a href=""></a>  {value}
-            </li>
-          )}
-        </ul>
-        <Bars3Icon className='md:hidden h-[32px] w-[32px] ' />
-        <div className="hidden md:block">
-           <ButtonComponent title={'Buy Now'} variant={'filled'} />
+    <Fragment>
+      <div className="flex flex-row gap-48 px-main-sides-phone md:px-main-sides-tablet 2xl:px-main-sides-desktop py-16 w-full backdrop-blur-sm sticky top-0 z-50">
+        <div className="flex items-center">
+          <img src="images/logo.svg" className='w-[200px]' alt="logo" />
         </div>
-       
-      </div>
-    </div >
+        <div className=" text-white w-full flex items-center justify-end gap-48">
+          <ul className='hidden text-b font-body md:flex flex-row gap-24 text-nav-item '>
+            {NAVIGATION.map((value, index) =>
+              <li key={index} className='hover:text-primary hover:cursor-pointer'>
+                <a href=""></a>  {value}
+              </li>
+            )}
+          </ul>
+          <Bars3Icon onClick={() => setOpenNav(true)} className='md:hidden h-[32px] w-[32px]' />
+
+          <div className="hidden md:block">
+            <ButtonComponent title={'Buy Now'} variant={'filled'} />
+          </div>
+        </div>
+      </div >
+
+      {openNav == true && <MobileMenuComponent close={() => setOpenNav(false)} />}
+    </Fragment>
   )
 }
