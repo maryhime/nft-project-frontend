0a2247c530c62a9847a6541ea2824b4bcba1ad84
Mary Caseria <mtmc@newmediastaff.com>
2023-09-14 10:45:40 +0800

    refs #9 fixed accordion to collapse all closes #9

--- src/pages/sections/FAQsSection.jsx
+++ src/pages/sections/FAQsSection.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import { QUESTIONS } from '../../data'
 import { Accordion } from 'flowbite-react'
-import { PlusIcon } from '@heroicons/react/24/solid'
 
 
-export const FAQsSection = () => {
+const FAQsSection = () => {
   return (
     <div className="flex flex-col lg:flex-row 2xl:gap-64 px-main-sides-phone md:px-main-sides-tablet 2xl:px-main-sides-desktop py-[10%] w-full gap-48">
       <h2 className="w-full text-white text-phone-main-heading md:text-tab-main-heading 2xl:text-main-heading uppercase">
         Your <span className='text-secondary'> Questions, </span>
         answered.
       </h2>
-      <Accordion  className=" flex flex-col gap-24 w-full text-white font-body">
+      <Accordion collapseAll className=" flex flex-col gap-24 w-full text-white font-body">
         {QUESTIONS.map((value, index) =>
           <Accordion.Panel key={index}>
-            <Accordion.Title className='hover:cursor-pointer justify-between text-body-text font-[600] lg:text-accordion-heading border-secondary  border-2 rounded-accordion open:rounded-accordion-open p-[16px] lg:px-24 lg:py-[20px]'>
+            <Accordion.Title className='hover:cursor-pointer justify-between text-body-text font-[600] lg:text-accordion-heading border-secondary  border-2 rounded-accordion p-[16px] lg:px-24 lg:py-[20px]'>
               {value.question}
             </Accordion.Title>
             <Accordion.Content className='px-24'>
@@ -24,10 +23,7 @@ export const FAQsSection = () => {
           
         )}
       </Accordion>
-
-
-
-
     </div>
   )
 }
+export default FAQsSection
