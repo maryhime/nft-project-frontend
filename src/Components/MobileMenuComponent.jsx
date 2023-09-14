44a5afd47096a8af25c10de88109945cb90beb61
Mary Caseria <mtmc@newmediastaff.com>
2023-09-14 13:11:23 +0800

    fixed css for cursor

--- src/Components/MobileMenuComponent.jsx
+++ src/Components/MobileMenuComponent.jsx
@@ -5,23 +5,23 @@ import { ButtonComponent } from './GenericComponent.jsx/ButtonComponent'
 
 export const MobileMenuComponent = ({ close }) => {
   return (
-    <div className="fixed z-50 top-0 w-screen bg-[#171217] h-full overflow-hidden px-main-sides-phone py-main-sides-phone flex flex-col gap-64 ">
+    <div className="fixed z-50 top-0 w-screen bg-[#171217] h-full overflow-hidden px-main-sides-phone py-main-sides-tablet flex flex-col gap-24 ">
       <div className="flex justify-end">
         <XMarkIcon onClick={close} className='h-[32px] w-[32px] text-white' />
       </div>
-      <div className="flex flex-col gap-48">
-          <ul className='text-[20px] text-white font-body  uppercase flex flex-col justify-center gap-24 '>
-        {NAVIGATION.map((value, index) =>
-          <li key={index} className='hover:text-primary hover:cursor-pointer text-center'>
-            <a href=""></a>  {value}
-          </li>
-        )}
-      </ul>
-      <div className="flex justify-center">
-        <ButtonComponent title={'Buy Now'} variant={'filled'} style={'w-[180px]'} />
+      <div className="flex flex-col gap-48 h-full justify-center">
+        <ul className='text-[20px] text-white font-body  uppercase flex flex-col justify-center gap-24 '>
+          {NAVIGATION.map((value, index) =>
+            <li key={index} className='hover:text-primary hover:cursor-pointer text-center'>
+              <a href=""></a>  {value}
+            </li>
+          )}
+        </ul>
+        <div className="flex justify-center">
+          <ButtonComponent title={'Buy Now'} variant={'filled'} style={'w-[180px]'} />
+        </div>
       </div>
-      </div>
-    
+
 
     </div>
   )
